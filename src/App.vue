7a10a14efcea69a067e4da2ec5a049028a78feeb
alsuhaibi96 <alsuhaibiabdulrahman@gmail.com>
2023-10-17 11:51:30 +0300

    add: button, description

---
+++ src/App.vue
@@ -0,0 +1,106 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+import ToDoTasks from './components/To-Do-Tasks.vue'
+</script>
+
+<template>
+  <header>
+    <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
+
+    <div >
+      <!-- <HelloWorld msg="You did it!" /> -->
+
+      <nav>
+     
+      </nav>
+      <ToDoTasks>
+    Study English
+    <template #button-text>
+Add to list
+    </template>
+    <template #description>
+hello desc
+    </template>
+      </ToDoTasks>
+      <ToDoTasks>
+    Study Math
+    <template #button-text>
+Add to list
+    </template>
+    <template #description>
+hello there
+    </template>
+      </ToDoTasks>
+      <div>
+      </div>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
